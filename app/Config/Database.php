6e5c04cd25bc841e01d0561c2db4f3434b0a068f
florian savoie <florianpsp303@gmail.com>
2023-07-09 22:03:22 +0000

    maj

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '!!',
-        'database' => '',
+        'username' => 'u572775118_webtools',
+        'password' => 'Florianserveur13!!',
+        'database' => 'u572775118_webtools',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
